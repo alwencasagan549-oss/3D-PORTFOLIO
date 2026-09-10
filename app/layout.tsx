a0220bcd06ae0d9fa86c5af2084aef3525111e0d
Alwen <alwencasagan549@gmail.com>
2026-09-10 20:11:22 +0800

    fix: add high-res favicon PNGs to reduce blur

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${orbitron.variable} h-full antialiased`}
     >
       <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="preconnect" href="https://prod.spline.design" />
       </head>
       <body className="min-h-full flex flex-col bg-black">
